e6cf53dc8a8aff6baf626c617048b9a7bbb8e3a8
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-01-06 00:52:29 +0330

    edit/ add parts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Card, Chart, Rightbar, Transactions } from '@/components';
+import React from 'react';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className='flex gap-5 mt-5'>
+      <div className='flex-[3] flex flex-col gap-5'>
+        <div className='flex gap-5 justify-between'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
 
-export default Dashboard
+      <div className='flex-1'>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
