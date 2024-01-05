dab360629e674bde49dfade4df17de263b7186cd
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-01-06 00:52:46 +0330

    add/ add another components

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,8 @@
-import Navbar from './dashboard/navbar/Navbar';
-import Sidebar from './dashboard/sidebar/Sidebar';
+import Navbar from './dashboard/navbar';
+import Sidebar from './dashboard/sidebar';
+import Chart from './dashboard/chart';
+import Card from './dashboard/card';
+import Rightbar from './dashboard/rightbar';
+import Transactions from './dashboard/transactions';
 
-export { Navbar, Sidebar };
+export { Navbar, Sidebar, Chart, Card, Rightbar, Transactions };
