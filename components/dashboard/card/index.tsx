b9f874c0876c714a10efeab83918be534871aa8a
msalean <alimohammadi13790830@gmail.com>
2024-01-06 12:06:15 +0330

    edit/ complete card

--- components/dashboard/card/index.tsx
+++ components/dashboard/card/index.tsx
@@ -3,13 +3,13 @@ import { MdSupervisedUserCircle } from 'react-icons/md';
 
 const Card = () => {
   return (
-    <div>
+    <div className='bg-bgSoft hover:bg-hoverbg p-5 rounded-[10px] flex gap-5 cursor-pointer w-full'>
       <MdSupervisedUserCircle size={24} />
-      <div>
+      <div className='flex flex-col'>
         <span className=''>Total Users</span>
-        <span className=''>10.273</span>
-        <span className=''>
-          <span className=''>12%</span> more than previous week
+        <span className='text-[24px] font-[500]'>10.273</span>
+        <span className='text-[14px] font-[300]'>
+          <span className='text-lime-600'>12%</span> more than previous week
         </span>
       </div>
     </div>
