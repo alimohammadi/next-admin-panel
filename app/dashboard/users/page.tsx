3a16de1014bf6af2725ab7530d76f8b5fe62d96d
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-01-09 07:34:15 +0330

    feature/ initialize users page

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { SearchBar } from '@/components';
+import Link from 'next/link';
+import React from 'react';
 
 const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <div className='mt-5 bg-bgSoft p-5 rounded-[10px]'>
+      <div className='flex items-center justify-between'>
+        <SearchBar placeholder='Search for a user...' />
+        <Link href='/dashboard/users/add' />
+        <button className='p-[10px] bg-[#5d57c9] text-text border-none rounded-[5px]'>
+          Add New
+        </button>
+      </div>
+      <table></table>
+    </div>
+  );
+};
 
-export default Users
+export default Users;
