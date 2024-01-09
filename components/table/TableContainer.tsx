e9ca50067813ee7209437fb3e327cc834908b6be
msalean <alimohammadi13790830@gmail.com>
2024-01-09 20:11:52 +0330

    feature/ create table container

---
+++ components/table/TableContainer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+const tableData = [
+  {
+    _id: '65004fef2add3e275fd79b75',
+    phoneNumber: '09045292582',
+    amount: 10000,
+    payed: true,
+    createdAt: '2023-09-12T11:47:59.581Z',
+  },
+  {
+    _id: '650164813fbdd79915f0566b',
+    phoneNumber: '09128995907',
+    amount: 10000,
+    payed: true,
+    createdAt: '2023-09-13T07:28:01.390Z',
+  },
+  {
+    _id: '6501677292f0834f92821f52',
+    phoneNumber: '09128995907',
+    amount: 10000,
+    payed: true,
+    createdAt: '2023-09-13T07:40:34.603Z',
+  },
+  {
+    _id: '65030382b8560a389ac48a3f',
+    phoneNumber: '09045292582',
+    amount: 10000,
+    payed: true,
+    createdAt: '2023-09-14T12:58:42.516Z',
+  },
+];
+
+const TableContainer = () => {
+  // onMouseDown={() => {}}
+  // onTouchStart={() => {}}
+  return (
+    <table>
+      <thead></thead>
+    </table>
+  );
+};
+
+export default TableContainer;
