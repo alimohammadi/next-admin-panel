602d34aab231a35bce40a6a4a4bf4720969d526b
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-01-09 07:33:41 +0330

    add/ add searchbar and footer component

--- components/index.ts
+++ components/index.ts
@@ -4,5 +4,16 @@ import Chart from './dashboard/chart';
 import Card from './dashboard/card';
 import Rightbar from './dashboard/rightbar';
 import Transactions from './dashboard/transactions';
+import Footer from './dashboard/footer';
+import SearchBar from './dashboard/searchBar';
 
-export { Navbar, Sidebar, Chart, Card, Rightbar, Transactions };
+export {
+  Navbar,
+  Sidebar,
+  Chart,
+  Card,
+  Rightbar,
+  Transactions,
+  Footer,
+  SearchBar,
+};
