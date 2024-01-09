61471ceeae95ae3e08254bc7d24e78192d311e34
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-01-09 07:32:54 +0330

    feature/ create footer

---
+++ components/dashboard/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-between mt-[30px] text-textSoft'>
+      <div className='font-bold'>Remitation</div>
+      <div className='text-[12px]'>© All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
